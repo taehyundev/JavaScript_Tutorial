041fb1dc79f84cb74c8590aded6766ce1f18eeaf
taehyundev <K.anon.11107@gmail.com>
2020-03-09 20:28:08 +0900

    update 2020-03-09

--- Node.JS/app.js
+++ Node.JS/app.js
@@ -1,6 +1,8 @@
 var express = require('express')
 //require로 express에 관련된 함수를 가져옴
 var app = express()
+var bodyParser = require('body-parser');
+
 //아래라인이 모두 실행되고 나서 listen 진행
 //아래는 비동기적
 app.listen(3000, function(){
@@ -10,6 +12,14 @@ app.listen(3000, function(){
 app.use(express.static('public'))
 //정적인 파일을 사용하기위해서 
 //main.js같은 파일을 요청하면 받을 수 있게
+
+app.use(bodyParser.json()) // json형태로 받을 때
+app.use(bodyParser.urlencoded({extended:true})) //그냥 post로
+//ASCII 형태로만 주고 받을 수 있어서 인코딩함
+app.set('view engine', 'ejs')
+// view engine은 ejs를 쓰겠다라고 설치 후 셋팅
+
+
 
 app.get('/', function(req,res){
   //  res.send("<h1>hi friend!</h1>"); //웹페이지에 값이 뜨게됨
@@ -21,6 +31,22 @@ app.get('/', function(req,res){
 app.get('/main', function(req,res){ // url /main으로 갔을 때
     res.sendfile(__dirname + "/public/main.html");
 });
+
+
+app.post('/email_post', function(req,res){ //req 요청 객체 //res 응답객체
+    //get : req, param("email") 
+    console.log(req.body.email);
+   //res.send("<h1>welcome ! " + req.body.email + "</h1>");
+    res.render('email.ejs', {'email' : req.body.email})
+    //render를 통해서 email.ejs 파일에 email이라는 변수의 값을 req.body.email로 하겠다.
+    //ejs는 템플릿엔진
+})
+
+app.post('/ajax_send_email', function(req,res){
+    console.log(req.body.email);
+    var responseData = {'result' : 'ok', 'email': req.body.email};
+    res.json(responseData);
+})
 
 /*
 
